c47863a0638549ebf2a8fcf86e88b22cdfb922c3
MpMp <mpmp@mpmp-desktop.(none)>
2012-06-20 21:43:14 +0200

    Version 0.1

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 jquery-tooltip
 ==============
 
-A jquery plugin for show elegance tooltips.
+A simple jquery plugin for show elegance tooltips.
+
+
+HOWTO
+=====
+
+Insert into head of html page the code above:
+
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="jquery-tooltip.js"></script>
+<style type="text/css">
+    @import url(jquery-tooltip.css);
+</style>
+
+Set the attributes "id" and "title" of the element where you want to show
+the tooltip.
+Set the attribute "class" of the same element at "jquery-tooltip-parent".
+
+Enjoy!
+
+See the example.html file for more info.
---
+++ jquery-tooltip.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    var tooltipId="#jquery-tooltip";
+    
+    $('.jquery-tooltip-parent').mouseover(function() {
+        tooltipId=tooltipId+"_"+$(this).attr('id');
+        //alert(tooltipId);
+        
+        // Retrieve the text to display in tooltip
+        var text=$(this).attr("title");
+        
+        $(this).attr( "tooltip_text", text );
+        
+        // Remove the value from the attribute title
+        $(this).attr("title", "");
+        
+        // Create the html of the tooltip
+        var tooltipHtml='<div class="jquery-tooltip" id="'+tooltipId.replace("#", "")+'" style="display:none">'+text+'</div>';
+        
+        // Append the tooltip
+        $('body').append(tooltipHtml);
+        
+        // Set the tooltip position
+        var elementTop=$(this).position().top;
+        var elementHeight=$(this).height();
+        var elementMarginTop=parseInt( $(this).css("marginTop").replace(/px/, "") );
+        var elementMarginBottom=parseInt( $(this).css("marginBottom").replace(/px/, "") );
+        var top=elementTop+elementHeight+elementMarginTop+elementMarginBottom;
+        
+        var elementWidth=$(this).width();
+        var elementLeft=$(this).position().left;
+        var tooltipWidth=$(tooltipId).width();
+        var windowWidth=$('body').width();
+        var left=elementLeft+(elementWidth/2)-(tooltipWidth/2);
+        var tooltipPaddingRight=parseInt( $(tooltipId).css('paddingRight').replace(/px/, "") );
+        var tooltipMarginRight=parseInt( $(tooltipId).css('marginRight').replace(/px/, "") );
+        var tooltipPaddingLeft=parseInt( $(tooltipId).css('paddingLeft').replace(/px/, "") );
+        var tooltipMarginLeft=parseInt( $(tooltipId).css('marginLeft').replace(/px/, "") );
+        
+        // If tooltip is out of the right edge of window
+        if( left+tooltipWidth > windowWidth )
+        {
+            left=windowWidth-tooltipWidth-tooltipPaddingRight-tooltipMarginRight-tooltipPaddingLeft-tooltipMarginLeft;
+        }
+        
+        // If tooltip is out of the left edge of window
+        if( left< 0 )
+        {
+            left=0;
+        }
+        
+        $(tooltipId).css("left", left);
+        $(tooltipId).css("top", top);
+        
+        $(tooltipId).fadeIn("slow");
+        
+    }).mouseout(function() {
+        var text=$(this).attr( "tooltip_text" );
+        
+        // Set the title attribute to old value
+        $(this).attr("title", text);
+        
+        $(tooltipId).fadeOut("slow", function(){
+            // Remove tooltip
+            $(this).remove(); 
+        });
+    });
+});
